0d05d35637affffb63ea8d06a63a5ff5eae55d21
Alexander DeFuria <defuria.alexander@gmail.com>
2022-11-17 15:31:20 -0500

    Now utilizing queues before looking for next event

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <queue>
 #include <random>
+#include <iomanip>
 #include "Event.h"
 
 #define SERVER_COUNT 4;
@@ -44,6 +45,8 @@ int main() {
 }
 
 void initialization() {
+    cout << setprecision(4);
+
     MeanInterArrivalTime = 7.523;
     SIGMAInterArrivalTime = 10.244;
     SIGMA[0] = 2.018;
@@ -61,7 +64,7 @@ void initialization() {
     for (int i = 0; i < 4; i++)
         ServerDistributions[i] = normal_distribution<>(MeanServiceTime[i], SIGMA[i]);
 
-    for (int i = 0; i < 50; i++) {
+    for (int i = 0; i < 10; i++) {
         Event firstEvent(arrival, normal(ArrivalDistribution), 0, TotalCustomers);
         TotalCustomers++;
         FutureEventList.push(firstEvent);
@@ -71,16 +74,14 @@ void initialization() {
 }
 
 // Push the departure event into the future
-void scheduleDeparture(Event event, int currentLocation) {
-    double ServiceTime = normal(ServerDistributions[event.getEventLocation()]);
-    Event depart = Event(departure, Clock+ServiceTime, currentLocation, event.getCustomerId());
+void scheduleDeparture(Event event, int currentServer) {
+    double ServiceTime = normal(ServerDistributions[currentServer]);
+    Event depart = Event(departure, Clock+ServiceTime, currentServer, event.getCustomerId());
     FutureEventList.push(depart);
-    NumberInService[currentLocation] = 1;
+    NumberInService[currentServer] = 1;
 }
 
 void processArrival(Event event) {
-    int selectedQueue;
-
     // Update the total busy time for each server.
     for (int i = 0; i < 4; i++) {
         if (NumberInService[i] == 1) {
@@ -95,25 +96,31 @@ void processArrival(Event event) {
                 scheduleDeparture(event, 0);
             else if (NumberInService[1] == 0)
                 scheduleDeparture(event, 1);
+            else
+                Queues[0].push(event);
             break;
         case 1:
             if (NumberInService[2] == 0)
                 scheduleDeparture(event, 2);
+            else
+                Queues[1].push(event);
             break;
         case 2:
             if (NumberInService[3] == 0)
                 scheduleDeparture(event, 3);
+            else
+                Queues[2].push(event);
             break;
         default:
             // At start of system
             event.setEventLocation(0);
             Queues[0].push(event);
             QueueLength[0]++;
-            selectedQueue = 0;
             scheduleDeparture(event, 0);
     }
 
     LastEventTime = Clock;
+    //if (event.getCustomerId() == 5)
 
     cout << "Time: " << Clock <<" :\t customer " << event.getCustomerId() << " arrived in queue " << event.getEventLocation() << endl;
 }
@@ -121,21 +128,39 @@ void processArrival(Event event) {
 void processDeparture(Event event) {
     NumberInService[event.getEventLocation()]--;
 
-    NumberOfDepartures[event.getEventLocation()] ++ ;
+    NumberOfDepartures[event.getEventLocation()]++;
 
-    switch (event.getEventLocation()) {
+    int location = event.getEventLocation();
+
+    switch (location) {
         case 0:
             FutureEventList.push(Event(arrival, Clock, 1, event.getCustomerId()));
+            if (!Queues[0].empty()) {
+                Event evt = Queues[0].front();
+                Queues[0].pop();
+                processArrival(evt);
+            }
             break;
         case 1:
             FutureEventList.push(Event(arrival, Clock, 2, event.getCustomerId()));
+            if (!Queues[0].empty()) {
+                Event evt = Queues[0].front();
+                Queues[0].pop();
+                processArrival(evt);
+            }
             break;
+        case 2:
+        case 3:
+            if (!Queues[location - 1].empty()) {
+                Event evt = Queues[location - 1].front();
+                Queues[location - 1].pop();
+                processArrival(evt);
+            }
         default:
             break;
     }
 
-
-
+//  if (event.getCustomerId() == 5)
     cout << "Time: " << Clock <<" :\t customer " << event.getCustomerId() << " finished from server " << event.getEventLocation() << endl;
 
 }
